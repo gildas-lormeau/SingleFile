3153a2861ed9b05d65857d6b3f3abb747573bf87
Gildas lormeau <gildas.lormeau@gmail.com>
2011-04-22 23:39:32 +0200

    changed dev extension ID

--- WebContent/ui/scripts/bg/background.js
+++ WebContent/ui/scripts/bg/background.js
@@ -47,7 +47,7 @@
 	}
 
 	function process(tabId, url, processSelection) {
-		var SINGLE_FILE_CORE_EXT_ID = dev ? "oabofdibacblkhpogjinmdbcekfkikjc" : "jemlklgaibiijojffihnhieihhagocma";
+		var SINGLE_FILE_CORE_EXT_ID = dev ? /*"oabofdibacblkhpogjinmdbcekfkikjc"*/ "onlinihoegnbbcmeeocfeplgbkmoidla" : "jemlklgaibiijojffihnhieihhagocma";
 		detectExtension(SINGLE_FILE_CORE_EXT_ID, function(detected) {
 			if (detected) {
 				if (processable(url)) {
