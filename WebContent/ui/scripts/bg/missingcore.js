afb87a44ae37a0c2600695fc35017becefecb7ce
Gildas lormeau <gildas.lormeau@gmail.com>
2013-06-22 01:46:08 +0200

    updated singlefile-core link

--- WebContent/ui/scripts/bg/missingcore.js
+++ WebContent/ui/scripts/bg/missingcore.js
@@ -1,3 +1,3 @@
 if (navigator.vendor.indexOf("Opera") != -1) {
-	document.getElementById("singlefile-core-link").href = "https://addons.opera.com/en/search/?query=singlefile+core";
+	document.getElementById("singlefile-core-link").href = "https://addons.opera.com/fr/extensions/details/singlefile-core";
 }
