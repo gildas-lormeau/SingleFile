610651fb670294115bd025092eed98195290ffed
Gildas <gildas.lormeau@gmail.com>
2018-07-30 22:51:12 +0200

    extracted code related to lazy loading into lazy-loader.js

--- extension/core/bg/bg.js
+++ extension/core/bg/bg.js
@@ -40,6 +40,7 @@ singlefile.core = (() => {
 		"/lib/single-file/rules-minifier.js",
 		"/lib/single-file/htmlnano.js",
 		"/lib/single-file/parse-srcset.js",
+		"/lib/single-file/lazy-loader.js",
 		"/lib/single-file/single-file-core.js",
 		"/lib/single-file/single-file-browser.js",
 		"/lib/fetch/content/fetch.js",
