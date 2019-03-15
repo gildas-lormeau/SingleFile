e4171acd42614177fd2e0c60e859a2647cda43fc
Gildas <gildas.lormeau@gmail.com>
2019-03-16 00:30:50 +0100

    isEnabled return falsy if tab is not passed as parameter

--- extension/core/bg/autosave.js
+++ extension/core/bg/autosave.js
@@ -62,8 +62,10 @@ singlefile.autosave = (() => {
 	}
 
 	async function isEnabled(tab) {
-		const [tabsData, rule] = await Promise.all([singlefile.tabsData.get(), singlefile.config.getRule(tab.url)]);
-		return singlefile.util.isAllowedURL(tab.url) && Boolean(tabsData.autoSaveAll || (tabsData.autoSaveUnpinned && !tab.pinned) || (tabsData[tab.id] && tabsData[tab.id].autoSave)) && (!rule || rule.autoSaveProfile != singlefile.config.DISABLED_PROFILE_NAME);
+		if (tab) {
+			const [tabsData, rule] = await Promise.all([singlefile.tabsData.get(), singlefile.config.getRule(tab.url)]);
+			return singlefile.util.isAllowedURL(tab.url) && Boolean(tabsData.autoSaveAll || (tabsData.autoSaveUnpinned && !tab.pinned) || (tabsData[tab.id] && tabsData[tab.id].autoSave)) && (!rule || rule.autoSaveProfile != singlefile.config.DISABLED_PROFILE_NAME);
+		}
 	}
 
 	async function refreshTabs() {
