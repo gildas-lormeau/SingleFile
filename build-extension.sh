c56569e41b89ccdc053e33fb037f2d06226b5fc5
Gildas <gildas.lormeau@gmail.com>
2019-03-20 02:25:58 +0100

    create separate builds for chromium and firefox

--- build-extension.sh
+++ build-extension.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-zip -r singlefile-extension.zip manifest.json extension lib _locales
+rm singlefile-extension-firefox.zip singlefile-extension-chromium.zip
+zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales
+cp manifest.json manifest.copy.json
+jq "del(.applications,.sidebar_action,.permissions[0],.options_ui.browser_style)" manifest.copy.json > manifest.json
+zip -r singlefile-extension-chromium.zip manifest.json extension lib _locales
+mv manifest.copy.json manifest.json
