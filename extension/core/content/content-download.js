fe493d5fe2d5bb3ab55ade900ff30762eac07234
Gildas <gildas.lormeau@gmail.com>
2019-04-16 22:00:54 +0200

    added IIFE

--- extension/core/content/content-download.js
+++ extension/core/content/content-download.js
@@ -23,16 +23,20 @@
 
 /* global addEventListener, removeEventListener, document, URL, MouseEvent, Blob */
 
-addEventListener("message", listener, false);
+(() => {
 
-function listener(event) {
-	removeEventListener("message", listener, false);
-	const [filename, content] = JSON.parse(event.data);
-	const link = document.createElement("a");
-	document.body.appendChild(link);
-	link.download = filename;
-	const url = URL.createObjectURL(new Blob([content], { type: "text/html" }));
-	link.href = url;
-	link.dispatchEvent(new MouseEvent("click"));
-	URL.revokeObjectURL(url);
-}
+	addEventListener("message", listener, false);
+
+	function listener(event) {
+		removeEventListener("message", listener, false);
+		const [filename, content] = JSON.parse(event.data);
+		const link = document.createElement("a");
+		document.body.appendChild(link);
+		link.download = filename;
+		const url = URL.createObjectURL(new Blob([content], { type: "text/html" }));
+		link.href = url;
+		link.dispatchEvent(new MouseEvent("click"));
+		URL.revokeObjectURL(url);
+	}
+
+})();
