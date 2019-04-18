d5c24d8f02ac730fe0e1aba437adb6029cee791c
Gildas <gildas.lormeau@gmail.com>
2019-04-18 20:44:00 +0200

    added eslint comments

--- extension/core/bg/download.js
+++ extension/core/bg/download.js
@@ -92,8 +92,8 @@ singlefile.download = (() => {
 				} else if (invalidFilename && page.filename.includes(",")) {
 					page.filename = page.filename.replace(/,/g, "_");
 					return downloadPage(page, { confirmFilename: options.confirmFilename, incognito: options.incognito, filenameConflictAction: options.filenameConflictAction });
-				} else if (invalidFilename && !page.filename.match(/^[\x00-\x7F]+$/)) {
-					page.filename = page.filename.replace(/[^\x00-\x7F]/g, "_");
+				} else if (invalidFilename && !page.filename.match(/^[\x00-\x7F]+$/)) { // eslint-disable-line  no-control-regex
+					page.filename = page.filename.replace(/[^\x00-\x7F]+/g, "_"); // eslint-disable-line  no-control-regex
 				} else if ((errorMessage.includes("'incognito'") || errorMessage.includes("\"incognito\"")) && options.incognito) {
 					return downloadPage(page, { confirmFilename: options.confirmFilename, filenameConflictAction: options.filenameConflictAction });
 				} else if (errorMessage == "conflictaction prompt not yet implemented" && options.filenameConflictAction) {
