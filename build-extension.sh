a04394b2c913969964349f759dc006961ed22439
Gildas <gildas.lormeau@gmail.com>
2019-05-01 19:35:20 +0200

    updated build script

--- build-extension.sh
+++ build-extension.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm singlefile-extension-firefox.zip singlefile-extension-chromium.zip
-zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales
+zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales index.js
 cp manifest.json manifest.copy.json
 jq "del(.applications,.permissions[0],.options_ui.browser_style)" manifest.copy.json > manifest.json
 zip -r singlefile-extension-chromium.zip manifest.json extension lib _locales
