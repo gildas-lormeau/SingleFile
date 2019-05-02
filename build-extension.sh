6a24d6b0927cd663d8646741666128d834baedc1
Gildas <gildas.lormeau@gmail.com>
2019-05-02 11:47:44 +0200

    fixed build script for Chrome

--- build-extension.sh
+++ build-extension.sh
@@ -3,5 +3,5 @@ rm singlefile-extension-firefox.zip singlefile-extension-chromium.zip
 zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales index.js
 cp manifest.json manifest.copy.json
 jq "del(.applications,.permissions[0],.options_ui.browser_style)" manifest.copy.json > manifest.json
-zip -r singlefile-extension-chromium.zip manifest.json extension lib _locales
+zip -r singlefile-extension-chromium.zip manifest.json extension lib _locales index.js
 mv manifest.copy.json manifest.json
