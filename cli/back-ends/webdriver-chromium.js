4d4ac9db2ab7bd5a711e1310a3175b7ddb6d28c9
Gildas <gildas.lormeau@gmail.com>
2019-07-26 20:10:02 +0200

    added content-hooks.js (fix #247)
    Former-commit-id: 468ed60910d28d3bffb71419ce9ae6dd8286dab7

--- cli/back-ends/webdriver-chromium.js
+++ cli/back-ends/webdriver-chromium.js
@@ -31,6 +31,7 @@ const { Builder } = require("selenium-webdriver");
 
 const SCRIPTS = [
 	"../../index.js",
+	"../../lib/hooks/content/content-hooks.js",
 	"../../lib/hooks/content/content-hooks-frames.js",
 	"../../lib/frame-tree/content/content-frame-tree.js",
 	"../../lib/lazy/content/content-lazy-loader.js",
