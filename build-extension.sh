8aa4d0fad61dd6d1ea48f2d8079f2cc1cf3c3a2d
Gildas <gildas.lormeau@gmail.com>
2019-07-31 14:29:55 +0200

    fixed warnings in Firefox (cf. about:debugging)
    Former-commit-id: bb85dd7fab8970ed265cbd02d6a444ef41e16952

--- build-extension.sh
+++ build-extension.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rm singlefile-extension-firefox.zip singlefile-extension-chromium.zip
-zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales index.js
 cp manifest.json manifest.copy.json
+jq "del(.options_page,.background.persistent)" manifest.copy.json > manifest.json
+zip -r singlefile-extension-firefox.zip manifest.json extension lib _locales index.js
 jq "del(.applications,.permissions[0],.options_ui.browser_style)" manifest.copy.json > manifest.json
 zip -r singlefile-extension-chromium.zip manifest.json extension lib _locales index.js
 mv manifest.copy.json manifest.json
