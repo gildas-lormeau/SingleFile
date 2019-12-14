7469f1c48386b4fad0d35065bf70e44c5ea18b61
Gildas <gildas.lormeau@gmail.com>
2019-12-14 02:41:38 +0100

    better fix of issue #318

--- extension/lib/single-file/frame-tree/bg/frame-tree.js
+++ extension/lib/single-file/frame-tree/bg/frame-tree.js
@@ -28,7 +28,7 @@
 	"use strict";
 
 	browser.runtime.onMessage.addListener((message, sender) => {
-		if (message.method == "singlefile.frameTree.initResponse") {
+		if (message.method == "singlefile.frameTree.initResponse" || message.method == "singlefile.frameTree.ackInitRequest") {
 			browser.tabs.sendMessage(sender.tab.id, message, { frameId: 0 });
 			return Promise.resolve({});
 		}
