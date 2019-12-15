f0c386b1ee83f0f71b72e4c759a72daa626482b5
Gildas <gildas.lormeau@gmail.com>
2019-12-15 02:24:00 +0100

    inject css-unescape in frames

--- extension/lib/single-file/core/bg/scripts.js
+++ extension/lib/single-file/core/bg/scripts.js
@@ -57,6 +57,7 @@ singlefile.extension.lib.core.bg.scripts = (() => {
 	const frameScriptFiles = [
 		"lib/single-file/index.js",
 		"lib/single-file/single-file-helper.js",
+		"lib/single-file/vendor/css-unescape.js",
 		"lib/single-file/processors/hooks/content/content-hooks-frames.js",
 		"lib/single-file/processors/frame-tree/content/content-frame-tree.js",
 		"extension/lib/single-file/index.js",
