9387f36870efe5cf0963cf6ebeb56cacd7efd62b
Gildas <gildas.lormeau@gmail.com>
2020-08-03 17:16:41 +0200

    circumvent evaluateOnNewDocument issue

--- cli/back-ends/puppeteer.js
+++ cli/back-ends/puppeteer.js
@@ -151,6 +151,7 @@ async function pageGoto(page, options) {
 		waitUntil: options.browserWaitUntil || NETWORK_IDLE_STATE
 	};
 	if (options.content) {
+		await page.goto("about:blank", loadOptions);
 		await page.setContent(options.content, loadOptions);
 	} else {
 		await page.goto(options.url, loadOptions);
