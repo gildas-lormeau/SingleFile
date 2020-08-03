682cff225ccfb5e9e093c0e7cf088d9226e43820
Gildas <gildas.lormeau@gmail.com>
2020-08-04 00:21:15 +0200

    replaced "applications" with "browser_specific_settings"

--- build-extension.sh
+++ build-extension.sh
@@ -11,12 +11,12 @@ sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' extension/core/bg/co
 zip -r singlefile-extension-firefox.zip manifest.json common extension lib _locales
 mv config.copy.js extension/core/bg/config.js
 
-jq "del(.applications,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
 sed -i 's/207618107333-bktohpfmdfnv5hfavi1ll18h74gqi27v/207618107333-8fpm0a5h0lho1svrhdj21sbri3via774/g' manifest.json
 zip -r singlefile-extension-chromium.zip manifest.json common extension lib _locales
 
 cp extension/core/bg/config.js config.copy.js
-jq "del(.applications,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
 sed -i 's/207618107333-bktohpfmdfnv5hfavi1ll18h74gqi27v/207618107333-8fpm0a5h0lho1svrhdj21sbri3via774/g' manifest.json
 sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' extension/core/bg/config.js
 mkdir _locales.copy
