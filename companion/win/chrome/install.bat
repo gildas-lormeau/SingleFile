42cc098d1e9cf41f94b2011715e231867377479f
Gildas <gildas.lormeau@gmail.com>
2020-08-07 00:08:27 +0200

    add keys in HKEY_CURRENT_USER only

--- companion/win/chrome/install.bat
+++ companion/win/chrome/install.bat
@@ -1,2 +1,2 @@
 @echo off
-reg add "HKLM\Google\Chrome\NativeMessagingHosts\singlefile_companion" /ve /t REG_SZ /d "%~dp0\singlefile_companion.json" /f
+reg add "HKEY_CURRENT_USER\Software\Google\Chrome\NativeMessagingHosts\singlefile_companion" /ve /t REG_SZ /d "%~dp0\singlefile_companion.json" /f
--- companion/win/firefox/install.bat
+++ companion/win/firefox/install.bat
@@ -1,3 +1,2 @@
 @echo off
-reg add "HKCU\Software\Mozilla\NativeMessagingHosts\singlefile_companion" /ve /t REG_SZ /d "%~dp0\singlefile_companion.json" /f
-reg add "HKLM\Software\Mozilla\NativeMessagingHosts\singlefile_companion" /ve /t REG_SZ /d "%~dp0\singlefile_companion.json" /f
+reg add "HKEY_CURRENT_USER\Software\Mozilla\NativeMessagingHosts\singlefile_companion" /ve /t REG_SZ /d "%~dp0\singlefile_companion.json" /f
