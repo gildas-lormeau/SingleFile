8948efce035778676788e1236a9c78bb5bbcdc82
Gildas <gildas.lormeau@gmail.com>
2020-10-04 01:57:06 +0200

    fixed positioning issue in Chrome

--- extension/ui/content/content-ui-main.js
+++ extension/ui/content/content-ui-main.js
@@ -446,6 +446,7 @@ this.singlefile.extension.ui.content.main = this.singlefile.extension.ui.content
 		logsWindowElement.style.setProperty("min-height", "16px", "important");
 		logsWindowElement.style.setProperty("transition", "height 100ms", "important");
 		logsWindowElement.style.setProperty("will-change", "height", "important");
+		logsWindowElement.style.setProperty("inset-block", "auto");
 	}
 
 	function updateLog(id, textContent, textStatus, options) {
