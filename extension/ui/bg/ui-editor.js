864896fab02fc78f9a2e2846fbfb90778763104f
Gildas <gildas.lormeau@gmail.com>
2021-09-27 23:23:38 +0200

    fix toolbar scroll (fix #786)

--- extension/ui/bg/ui-editor.js
+++ extension/ui/bg/ui-editor.js
@@ -229,13 +229,11 @@ function toolbarOnTouchStart(event) {
 function toolbarOnTouchMove(event) {
 	if (toolbarPositionPointer != null) {
 		const position = getPosition(event);
-		toolbarTranslate = Math.min(Math.max((orientationPortrait ? position.screenY : position.screenX) - toolbarPositionPointer, toolbarTranslateMax), 0);
-		if (Math.abs(toolbarTranslate)) {
-			toolbarMoving = true;
-			toolbarElement.style.setProperty("transform", orientationPortrait ? `translate(0, ${toolbarTranslate}px)` : `translate(${toolbarTranslate}px, 0)`);
-			editorElement.style.setProperty("pointer-events", "none");
-			event.preventDefault();
-		}
+		toolbarTranslate = Math.min(Math.max((orientationPortrait ? position.screenY : position.screenX) - toolbarPositionPointer, toolbarTranslateMax - toolbarPositionPointer), 0);
+		toolbarMoving = true;
+		toolbarElement.style.setProperty("transform", orientationPortrait ? `translate(0, ${toolbarTranslate}px)` : `translate(${toolbarTranslate}px, 0)`);
+		editorElement.style.setProperty("pointer-events", "none");
+		event.preventDefault();
 	}
 }
 
