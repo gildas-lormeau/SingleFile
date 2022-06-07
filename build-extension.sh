08de366d2bf8e55b626ae245e3c05288b6044acd
Gildas <gildas.lormeau@gmail.com>
2022-06-07 15:44:12 +0200

    remove cli dependency from source zip file

--- build-extension.sh
+++ build-extension.sh
@@ -2,7 +2,10 @@
 
 npx rollup -c rollup.config.js
 
-zip -r singlefile-extension-source.zip manifest.json package.json package-lock.json _locales src rollup*.js .eslintrc.js build-extension.sh
+cp package.json package.copy.json
+jq 'del(.dependencies."single-file-cli")' package.copy.json > package.json
+zip -r singlefile-extension-source.zip manifest.json package.json _locales src rollup*.js .eslintrc.js build-extension.sh
+mv package.copy.json package.json
 
 rm singlefile-extension-firefox.zip singlefile-extension-chromium.zip singlefile-extension-edge.zip
 cp manifest.json manifest.copy.json
@@ -12,19 +15,19 @@ sed -i 's/000000000000000000000000/VQJ8Gq8Vxx72QyxPyeLtWvUt/g' src/core/bg/downl
 
 cp src/core/bg/config.js config.copy.js
 cp src/core/bg/companion.js companion.copy.js
-jq "del(.options_page,.background.persistent,.optional_permissions[0],.permissions[2],.oauth2)" manifest.copy.json > manifest.json
+jq "del(.options_page,.background.persistent,.optional_permissions[0],.permissions[2],.oauth2)" manifest.copy.json >manifest.json
 sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' src/core/bg/config.js
 sed -i 's/enabled: true/enabled: false/g' src/core/bg/companion.js
 zip -r singlefile-extension-firefox.zip manifest.json lib _locales src
 mv config.copy.js src/core/bg/config.js
 mv companion.copy.js src/core/bg/companion.js
 
-jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json >manifest.json
 sed -i 's/207618107333-3pj2pmelhnl4sf3rpctghs9cean3q8nj/207618107333-7tjs1im1pighftpoepea2kvkubnfjj44/g' manifest.json
 zip -r singlefile-extension-chromium.zip manifest.json lib _locales src
 
 cp src/core/bg/config.js config.copy.js
-jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json >manifest.json
 sed -i 's/207618107333-3pj2pmelhnl4sf3rpctghs9cean3q8nj/207618107333-7tjs1im1pighftpoepea2kvkubnfjj44/g' manifest.json
 sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' src/core/bg/config.js
 sed -i 's/image\/avif,//g' src/core/bg/config.js
