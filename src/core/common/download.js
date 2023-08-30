996324c70d5f6616b7baeea044fe81fba9d3e27a
Gildas <gildas.lormeau@gmail.com>
2023-08-30 23:35:41 +0200

    rollback

--- src/core/common/download.js
+++ src/core/common/download.js
@@ -65,14 +65,12 @@ async function downloadPage(pageData, options) {
 			defaultEditorMode: options.defaultEditorMode,
 			includeInfobar: options.includeInfobar,
 			warnUnsavedPage: options.warnUnsavedPage,
-			blobURL,
-			finished: true
+			blobURL
 		};
 		const result = await browser.runtime.sendMessage(message);
 		URL.revokeObjectURL(blobURL);
 		if (result.error) {
 			message.blobURL = null;
-			message.finished = null;
 			for (let blockIndex = 0; blockIndex * MAX_CONTENT_SIZE < pageData.content.length; blockIndex++) {
 				message.truncated = pageData.content.length > MAX_CONTENT_SIZE;
 				if (message.truncated) {
