3940275c3ebdabcfb7bd33e0db6d4544c9858d2c
Gildas <gildas.lormeau@gmail.com>
2024-09-03 11:21:21 +0200

    fixed useless code

--- src/core/bg/autosave.js
+++ src/core/bg/autosave.js
@@ -152,7 +152,6 @@ async function saveContent(message, tab) {
 		options.incognito = tab.incognito;
 		options.tabId = tabId;
 		options.tabIndex = tab.index;
-		options.saveToRestFormApi || options.saveToS3;
 		let pageData;
 		try {
 			if (options.autoSaveExternalSave) {
