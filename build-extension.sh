4685622770e308cee050aad71ed728012a579169
Gildas <gildas.lormeau@gmail.com>
2026-01-20 18:06:27 +0100

    fix build script (see #1905)

--- build-extension.sh
+++ build-extension.sh
@@ -25,15 +25,15 @@ npx rollup -c rollup.config.js
 
 cp package.json package.copy.json
 jq 'del(.dependencies."single-file-cli")' package.copy.json > package.json
-zip -r singlefile-extension-source.zip manifest.json package.json _locales src rollup*.js .eslintrc.js build-extension.sh
+zip -r singlefile-extension-source.zip manifest.json package.json _locales src rollup*.js eslint.config.mjs build-extension.sh
 mv package.copy.json package.json
 
-rm singlefile-extension-firefox.zip
+rm -f singlefile-extension-firefox.zip
 
 cp src/core/bg/config.js config.copy.js
 cp src/core/bg/companion.js companion.copy.js
-sed -i "" 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' src/core/bg/config.js
-sed -i "" 's/enabled: true/enabled: false/g' src/core/bg/companion.js
+node -e "const fs=require('fs');const file='src/core/bg/config.js';const updated=fs.readFileSync(file,'utf8').replace(/forceWebAuthFlow: false/g,'forceWebAuthFlow: true');fs.writeFileSync(file,updated);"
+node -e "const fs=require('fs');const file='src/core/bg/companion.js';const updated=fs.readFileSync(file,'utf8').replace(/enabled: true/g,'enabled: false');fs.writeFileSync(file,updated);"
 zip -r singlefile-extension-firefox.zip manifest.json lib _locales src
 mv config.copy.js src/core/bg/config.js
 mv companion.copy.js src/core/bg/companion.js
