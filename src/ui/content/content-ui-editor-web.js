98247cc15a363251f003e4324c1804c4f57dbcdb
clsty <celestial.y@outlook.com>
2026-08-12 22:25:15 +0800

    fix REST form API URL when saving from the editor
    Use the original page URL instead of the editor URL when submitting
    the url field to the REST form API.

--- src/ui/content/content-ui-editor-web.js
+++ src/ui/content/content-ui-editor-web.js
@@ -290,7 +290,7 @@ import { convert } from "../../lib/mhtml-to-html/mod.js";
 		};
 	}
 
-	async function init({ content, password, compressContent }, { filename, reset, isMHTML } = {}) {
+	async function init({ content, password, compressContent, url }, { filename, reset, isMHTML } = {}) {
 		await initConstants();
 		if (compressContent) {
 			const zipOptions = {
@@ -394,6 +394,9 @@ import { convert } from "../../lib/mhtml-to-html/mod.js";
 					formatPageEnabled: isProbablyReaderable(document)
 				}), "*");
 			}
+		}
+		if (!pageUrl && url) {
+			pageUrl = url;
 		}
 	}
 
